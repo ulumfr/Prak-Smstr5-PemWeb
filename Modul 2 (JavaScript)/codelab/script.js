e550192b2f792691cee57c7acca4db4a9f6cb604
Bahrul 'Ulum Fadhlur Rohman <bahrululumfr@gmail.com>
2023-10-07 02:13:16 +0700

    simple code

--- Modul 2 (JavaScript)/codelab/script.js
+++ Modul 2 (JavaScript)/codelab/script.js
@@ -1,13 +1,12 @@
+let satu = document.getElementById("pertama");
+let dua = document.getElementById("kedua");
+
 function jumlahkan() {
-    let satu = parseFloat(document.getElementById("pertama").value);
-    let dua = parseFloat(document.getElementById("kedua").value);
-
-    let hasil = satu + dua;
-
+    let hasil = parseFloat(satu.value) + parseFloat(dua.value);
     alert(`Hasil Penjumlahan = ${hasil}`)
 }
 
 function hapus() {
-    document.getElementById("pertama").value = "";
-    document.getElementById("kedua").value = "";
+    satu.value = "";
+    dua.value = "";
 }

59ae7513a4dee9ef674198c57ab804f29dde2b62
Bahrul 'Ulum Fadhlur Rohman <bahrululumfr@gmail.com>
2023-10-16 03:36:44 +0700

    add todo v2 (fix)

--- Modul 2 (JavaScript)/praktikum/tugas2/assets/js/script.js
+++ Modul 2 (JavaScript)/praktikum/tugas2/assets/js/script.js
@@ -1,37 +1,61 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const inputActivity = document.getElementById("inputActivity");
-    const addActivityButton = document.getElementById("addActivity");
-    const activityList = document.getElementById("activityList");
+const inputActivity = document.getElementById('inputActivity');
+const addActivityButton = document.getElementById('addActivity');
+const activityList = document.getElementById('activityList');
 
-    addActivityButton.addEventListener("click", function() {
-        const activityText = inputActivity.value.trim();
-        if (activityText !== "") {
-            const listItem = document.createElement("div");
-            listItem.innerHTML = `
-                <div class="d-flex justify-content-between align-items-center m-2">
-                    <span>${activityText}</span>
-                    <div>
-                        <button class="btn btn-primary btn-sm mx-1 edit-button"><i class='bx bx-edit'></i></button>
-                        <button class="btn btn-danger btn-sm delete-button"><i class='bx bxs-trash-alt'></i></button>
-                    </div>
+addActivityButton.addEventListener('click', function() {
+    const activityText = inputActivity.value.trim();
+
+    if (activityText === '') {
+        alert('Inputan tidak boleh kosong!!!');
+        return;
+    }
+
+    const activityItem = document.createElement('div');
+    activityItem.innerHTML = 
+        `<div class="d-flex justify-content-between align-items-center m-2">
+            <div class="input-group-text gap-2 w-100 bg-transparent p-2 rounded-1">
+                <input type="text" class="form-control rounded-2 bg-transparent border-0" value="${activityText}" disabled>
+                
+                <div>
+                    <button class="btn btn-success success-button rounded-2"><i class='bx bx-check'></i></button>
+                    <button class="btn btn-primary edit-button rounded-2"><i class='bx bx-edit'></i></button>
+                    <button class="btn btn-danger delete-button rounded-2"><i class='bx bxs-trash-alt'></i></button>
+                    <button class="btn btn-primary save-button rounded-2 d-none"><i class='bx bxs-save'></i></button>
                 </div>
-            `;
+                
+            </div>
+        </div>`;
 
-            activityList.appendChild(listItem);
-            inputActivity.value = "";
+    activityList.appendChild(activityItem);
+    inputActivity.value = '';
+    
+    const activityInput = activityItem.querySelector('input');
+    const successButton = activityItem.querySelector('.success-button');
+    const editButton = activityItem.querySelector('.edit-button');
+    const saveButton = activityItem.querySelector('.save-button');
+    const deleteButton = activityItem.querySelector('.delete-button');
 
-            const editButton = listItem.querySelector(".edit-button");
-            editButton.addEventListener("click", function() {
-                const newText = prompt("Edit activity:", activityText);
-                if (newText !== null) {
-                    listItem.querySelector("span").textContent = newText;
-                }
-            });
+    successButton.addEventListener('click', function() {
+        activityInput.classList.toggle('text-decoration-line-through');
+    });
 
-            const deleteButton = listItem.querySelector(".delete-button");
-            deleteButton.addEventListener("click", function() {
-                activityList.removeChild(listItem);
-            });
-        }
+    editButton.addEventListener('click', function() {
+        activityInput.removeAttribute('disabled');
+        saveButton.classList.remove('d-none');
+        successButton.classList.add('d-none');
+        editButton.classList.add('d-none');
+        deleteButton.classList.add('d-none');
+    });
+
+    saveButton.addEventListener('click', function() {
+        activityInput.setAttribute('disabled', 'true');
+        successButton.classList.remove('d-none');
+        editButton.classList.remove('d-none');
+        deleteButton.classList.remove('d-none');
+        saveButton.classList.add('d-none');
+    });
+
+    deleteButton.addEventListener('click', function() {
+        activityList.removeChild(activityItem);
     });
 });
